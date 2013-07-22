36f108349cf0869195adb6a298e7a5e131a8ba44
Lanny McNie <lanny@gskinner.com>
2013-07-22 16:34:24 -0600

    Updated NEXT docs and lib
    Signed-off-by: Lanny McNie <lanny@gskinner.com>

--- src/preloadjs/version.js
+++ src/preloadjs/version.js
@@ -25,6 +25,6 @@ o.version = /*version*/"NEXT"; // injected by build process
  * @type String
  * @static
  **/
-o.buildDate = /*date*/"Thu, 18 Jul 2013 19:44:33 GMT"; // injected by build process
+o.buildDate = /*date*/"Mon, 22 Jul 2013 22:31:32 GMT"; // injected by build process
 
 })();
