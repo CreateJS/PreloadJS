74d8c5850313f2f7a3ffd9bedc15c6b28903f909
Lanny McNie <lanny@gskinner.com>
2013-08-02 14:47:37 -0600

    Updated docs and lib
    Signed-off-by: Lanny McNie <lanny@gskinner.com>

--- src/preloadjs/version.js
+++ src/preloadjs/version.js
@@ -27,6 +27,6 @@ this.createjs = this.createjs||{};
 	 * @type String
 	 * @static
 	 **/
-	s.buildDate = /*date*/"Thu, 01 Aug 2013 16:39:39 GMT"; // injected by build process
+	s.buildDate = /*date*/"Fri, 02 Aug 2013 20:46:42 GMT"; // injected by build process
 
 })();
