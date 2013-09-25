99832ff9d01f4f581906462063bd6ecd9df195f8
Lanny McNie <lanny@gskinner.com>
2013-09-25 11:17:07 -0600

    PreloadJS version 0.4.0
    Signed-off-by: Lanny McNie <lanny@gskinner.com>

--- src/preloadjs/version.js
+++ src/preloadjs/version.js
@@ -19,7 +19,7 @@ this.createjs = this.createjs||{};
 	 * @type String
 	 * @static
 	 **/
-	s.version = /*version*/"NEXT"; // injected by build process
+	s.version = /*version*/"0.4.0"; // injected by build process
 
 	/**
 	 * The build date for this release in UTC format.
@@ -27,6 +27,6 @@ this.createjs = this.createjs||{};
 	 * @type String
 	 * @static
 	 **/
-	s.buildDate = /*date*/"Wed, 11 Sep 2013 18:34:28 GMT"; // injected by build process
+	s.buildDate = /*date*/"Wed, 25 Sep 2013 17:09:35 GMT"; // injected by build process
 
 })();
