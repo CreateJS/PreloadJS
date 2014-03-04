e8471683348ddd322adfd3e4ee24935e88a9628e
Wes  Gorgichuk <wes@gskinner.com>
2014-03-04 16:46:22 -0700

    Added .combined.js file.  Also updated grunt build to create it.

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -215,7 +215,7 @@ module.exports = function (grunt) {
 	 *
 	 */
 	grunt.registerTask('coreBuild', [
-		"updateversion", "uglify", "docs", "copy:src"
+		"updateversion", "combine", "uglify", "docs", "copy:src"
 	]);
 
 	/**
