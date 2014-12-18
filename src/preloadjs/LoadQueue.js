10c7c10f91d533667239360a6b9454c5104e609b
Lanny McNie <lanny@gskinner.com>
2014-12-18 11:02:20 -0700

    Added documentation to show "initialize" as REMOVED

--- src/preloadjs/LoadQueue.js
+++ src/preloadjs/LoadQueue.js
@@ -275,6 +275,20 @@ this.createjs = this.createjs || {};
 	var s = LoadQueue;
 
 	/**
+	 * <strong>REMOVED</strong>. Removed in favor of using `MySuperClass_constructor`.
+	 * See {{#crossLink "Utility Methods/extend"}}{{/crossLink}} and {{#crossLink "Utility Methods/promote"}}{{/crossLink}}
+	 * for details.
+	 *
+	 * There is an inheritance tutorial distributed with EaselJS in /tutorials/Inheritance.
+	 *
+	 * @method initialize
+	 * @protected
+	 * @deprecated
+	 */
+	// p.initialize = function() {}; // searchable for devs wondering where it is.
+
+
+	/**
 	 * An internal initialization method, which is used for initial set up, but also to reset the LoadQueue.
 	 * @method init
 	 * @param preferXHR
