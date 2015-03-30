695ca84993f68cb47490f9ed202d509154471ec8
Wes  Gorgichuk <wes@gskinner.com>
2015-03-30 16:37:13 -0600

    Updated unit tests to run off of the combined NEXT source.

--- tests/Gruntfile.js
+++ tests/Gruntfile.js
@@ -1,70 +1,88 @@
 module.exports = function (grunt) {
 	grunt.initConfig(
-		{
-			pkg: grunt.file.readJSON('package.json'),
+			{
+				pkg: grunt.file.readJSON('package.json'),
 
-			jasmine: {
-				run: {
-					src: [
-						'../lib/preloadjs-0.4.1.combined.js'
-					],
-					options: {
-						specs: 'spec/*Spec.js',
-						helpers: []
+				jasmine: {
+					run: {
+						src: [
+							'../lib/preloadjs-NEXT.combined.js',
+							'../lib/movieclip-NEXT.combined.js'
+						],
+
+						options: {
+							specs: 'spec/*Spec.js',
+							helpers: [
+								'spec/Helpers.js'
+							],
+							vendor: [
+								'../_assets/libs/easeljs-NEXT.min.js',
+								'../_assets/libs/preloadjs-NEXT.min.js'
+							],
+							host: 'http://127.0.0.1:<%=connect.serve.options.port%>/'
+						}
 					}
-				}
-			},
+				},
 
-			connect: {
-				server: {
-					options: {
-						keepalive: true,
-						base: ['../_assets/', '../lib/', '../', './'],
-						middleware: function (connect, options, middlewares) {
-							middlewares.unshift(function echo(req, res, next) {
-								if (req.method == "POST") {
-									res.end(JSON.stringify(req.body));
-								} else {
-									next();
-								}
-							});
-
-							var bodyParser = require('body-parser')
-							middlewares.unshift(bodyParser.json());
-							middlewares.unshift(bodyParser.urlencoded({
-							  extended: true
-							}));
-
-							return middlewares;
-						},
-					}
-				}
-			},
-
-			findopenport: {
 				connect: {
-					options: {
-						ports: [8000, 8888, 9000, 9999, 9001, 8001],
-						configName: "connect.server.options.port"
-					}
-				}
-			},
+					serve: {
+						options: {
+							keepalive: true,
+							base: [
+								{
+									path: __dirname,
+									options: {
+										index: '_SpecRunner.html'
+									}
+								}, '..', '../_assets/', '../lib/', './'
+							],
+							useAvailablePort: true,
+							port: 8000,
+							open: true,
+							// Used to test the POST functionality, it just echo's back what data was sent.
+							middleware: function (connect, options, middlewares) {
+								middlewares.unshift(function echo (req, res, next) {
+									if (req.method == "POST") {
+										res.end(JSON.stringify(req.body));
+									} else {
+										next();
+									}
+								});
+								var bodyParser = require('body-parser')
+								middlewares.unshift(bodyParser.json());
+								middlewares.unshift(bodyParser.urlencoded({
+									extended: true
+								}));
 
-			listips: {
-				run: {
-					options: {
-						port: "<%=connect.server.options.port %>",
-						label: "Normal"
+								return middlewares;
+							},
+						}
+					}
+				},
+
+				listips: {
+					run: {
+						options: {
+							label: "Normal"
+						}
 					}
 				}
 			}
-		}
 	);
+
+	grunt.registerTask('configureConnectHeadless', function () {
+		grunt.config('connect.serve.options.keepalive', false);
+		grunt.config('connect.serve.options.open', false);
+	});
 
 	// Load all the tasks we need
 	grunt.loadNpmTasks('grunt-contrib-jasmine');
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadTasks('tasks/');
 
-	grunt.registerTask("default", ["findopenport", "listips", "connect"]);
+	grunt.registerTask("default", "Launches browser-based tests", "serve");
+	grunt.registerTask("serve", "Launches browser-based tests", ["listips", "connect"]);
+
+	grunt.registerTask("headless", "phantom");
+	grunt.registerTask("phantom", "Launches phantom-based tests", ["configureConnectHeadless", "connect", "jasmine"]);
 };
